3ec88bc795650405ebf71925f20ed93792e76a9d
Mehade Hasan <40319670+Shmehade321@users.noreply.github.com>
2020-09-06 20:38:07 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCNA4ZqSKYVJIAK3GJlPTB-9JI6IdGTekY",
-  authDomain: "instagram-clone-react-93236.firebaseapp.com",
-  databaseURL: "https://instagram-clone-react-93236.firebaseio.com",
-  projectId: "instagram-clone-react-93236",
-  storageBucket: "instagram-clone-react-93236.appspot.com",
-  messagingSenderId: "21590736072",
-  appId: "1:21590736072:web:d5f5965bf5c6e151eef9fd",
-  measurementId: "G-PL1NWR7YLX",
+  apiKey: <your api key>,
+  authDomain: <your auth domain>,
+  databaseURL: <your database URL>,
+  projectId: <your project id>,
+  storageBucket: <your storage bucket>,
+  messagingSenderId: <your messaging sender id>,
+  appId: <your app id>,
+  measurementId: <your measurement id>,
 });
 
 const db = firebaseApp.firestore();
